1bea08563333723b6479116446c4f5cf86cfeb3a
David Pires <79417054+Dacops@users.noreply.github.com>
2023-10-16 00:16:16 +0100

    exercise-3

--- engine/samples/exercises/main.cpp
+++ engine/samples/exercises/main.cpp
@@ -11,15 +11,23 @@ Tasks:
     Exercise 2
     6-11. Set up ./assets
     12. Use ./assets
+
+    Revert to Exercise1 main.cpp 
+    Exercise 3
+    13. Add the input plugin
+    14-15. Create Input files in ./assets
+    16. Do something with the input (Show cube by pressing "s")
 */
 
 
 #include <cubos/engine/cubos.hpp>
 #include <cubos/engine/renderer/plugin.hpp>       // 1, 5
+#include <cubos/engine/voxels/grid.hpp>           // 3
 #include <cubos/engine/renderer/point_light.hpp>  // 4
 #include <cubos/engine/renderer/camera.hpp>       // 5
-#include <cubos/engine/settings/settings.hpp>     // 12
-#include <cubos/engine/voxels/plugin.hpp>         // 12
+#include <cubos/engine/input/plugin.hpp>          // 13
+#include <cubos/engine/settings/plugin.hpp>     // 16
+
 
 // include extra required components: Position, Rotation, LocalToWorld
 #include <cubos/engine/transform/plugin.hpp>
@@ -27,39 +35,30 @@ Tasks:
 using cubos::core::ecs::Commands;
 using cubos::core::ecs::Read;
 using cubos::core::ecs::Write;
+using cubos::core::ecs::Query;
 
 using namespace cubos::engine;
 
-// 12
-// Get assets
-static const Asset<VoxelGrid> CastleAsset = AnyAsset("2d9f71ff-3c5c-4a23-a8a2-ea1110148f0b");
-static const Asset<VoxelPalette> PaletteAsset = AnyAsset("2a0a78ac-8c4f-4e78-ba47-f99fb2b30cca");
+static const Asset<InputBindings> BindingsAsset = AnyAsset("7f36cf98-0f5e-4bf9-9325-066ea9550ebd");
 
 
-// 12, Set up "assets.io.path"
-static void settingsSystem(Write<Settings> settings)
-{
-    settings->setString("assets.io.path", SAMPLE_ASSETS_FOLDER);
-}
-
-
-// 2 -> 12, Load palette instead of creating one
-static void setPaletteSystem(Read<Assets> assets, Write<Renderer> renderer)
+// 2
+static void setPaletteSystem(Write<Renderer> renderer)
 {
     // Create a palette with 1 material (Red)
-    auto palette = assets->read(PaletteAsset);
-    (*renderer)->setPalette(*palette);
+    (*renderer)->setPalette(VoxelPalette{{
+        {{1, 0, 0, 1}},
+    }});
 }
 
-// 3 -> 12, Load voxel grid instead of creating one
-static void spawnCastleSystem(Commands commands, Read<Assets> assets)
+// 3
+static void spawnVoxelGridSystem(Commands commands, Write<Assets> assets)
 {
-    // Calculate the necessary offset to center the model on (0, 0, 0).
-    auto castle = assets->read(CastleAsset);
-    glm::vec3 offset = glm::vec3(castle->size().x, 0.0F, castle->size().z) / -2.0F;
+    // Create a 1x1x1 grid (Cube)
+    auto gridAsset = assets->create(VoxelGrid{{1, 1, 1}, {1}});
 
-    // Create the car entity
-    commands.create().add(RenderableGrid{CastleAsset, offset}).add(LocalToWorld{});
+    // Spawn an entity with a renderable grid component and a identity transform.
+    auto entity = commands.create(RenderableGrid{gridAsset, {-1.0F, 0.0F, -1.0F}}, LocalToWorld{});
 }
 
 // 4
@@ -67,8 +66,8 @@ static void spawnLightSystem(Commands commands)
 {
     // Spawn a point light.
     commands.create()
-        .add(PointLight{.color = {1.0F, 1.0F, 1.0F}, .intensity = 5.0F, .range = 100.0F})
-        .add(Position{{10.0F, 30.0F, -20.0F}});
+        .add(PointLight{.color = {1.0F, 1.0F, 1.0F}, .intensity = 5.0F, .range = 10.0F})
+        .add(Position{{1.0F, 3.0F, -2.0F}});
 }
 
 // 5
@@ -78,24 +77,55 @@ static void spawnCamerasSystem(Commands commands, Write<ActiveCameras> camera)
     camera->entities[0] =
         commands.create()
             .add(Camera{.fovY = 60.0F, .zNear = 0.1F, .zFar = 100.0F})
-            .add(Position{{-20.0, 10.0F, -30.0F}})
+            .add(Position{{-3.0, 1.0F, -3.0F}})
             .add(Rotation{glm::quatLookAt(glm::normalize(glm::vec3{1.0F, 0.0F, 1.0F}), glm::vec3{0.0F, 1.0F, 0.0F})})
             .entity();
 }
 
+// 16
+// Load bindings
+static void config(Write<Settings> settings)
+{
+    settings->setString("assets.io.path", SAMPLE_ASSETS_FOLDER);
+}
+static void init(Read<Assets> assets, Write<Input> input)
+{
+    auto bindings = assets->read<InputBindings>(BindingsAsset);
+    input->bind(*bindings);
+}
+
+// 16
+void showCube(Commands commands, Query<Read<RenderableGrid>> entities, 
+                Write<Assets> assets, Read<Input> input)
+{
+    // Destroy cube
+    for (auto [entity, position] : entities)
+    {
+        commands.destroy(entity);
+    }
+    // If key is pressed, spawn a new cube ("show" cube)
+    if (input->pressed("show-cube")) {
+        auto gridAsset = assets->create(VoxelGrid{{1, 1, 1}, {1}});
+        commands.create(RenderableGrid{gridAsset, {-1.0F, 0.0F, -1.0F}}, LocalToWorld{});
+    }
+}
+
+
 int main()
 {
      Cubos cubos{};
-     cubos.addPlugin(rendererPlugin);             // 1
-     cubos.addPlugin(voxelsPlugin);               // 12
+     cubos.addPlugin(rendererPlugin);                   // 1
+     cubos.addPlugin(inputPlugin);                      // 12
 
-     // 12
-     cubos.startupSystem(settingsSystem).tagged("cubos.settings");
+     // 2
      cubos.startupSystem(setPaletteSystem).after("cubos.renderer.init");
-     cubos.startupSystem(spawnLightSystem);       // 4
-     cubos.startupSystem(spawnCamerasSystem);     // 5
+     cubos.startupSystem(spawnVoxelGridSystem);         // 3
+     cubos.startupSystem(spawnLightSystem);             // 4
+     cubos.startupSystem(spawnCamerasSystem);           // 5
+     cubos.startupSystem(config).tagged("cubos.settings");  // 16
+     cubos.startupSystem(init).tagged("cubos.assets");  // 16
 
-     cubos.system(spawnCastleSystem);             // 12
+     cubos.system(showCube);                            // 16
 
      cubos.run();
 }
